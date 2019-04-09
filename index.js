929b11a7c702ea06b402295319f5a8520091b4a9
asoft <web.dev.asoft@gmail.com>
2019-04-09 18:02:27 +0300

    create package and script

---
+++ index.js
@@ -0,0 +1,24 @@
+'use strict';
+const fastbootTransform = require('fastboot-transform');
+
+
+module.exports = {
+    name: 'ember-lightbox-me',
+    options: {
+        nodeAssets: {
+            'lightbox_me': {
+                import: {
+                    include: ['jquery.lightbox_me.js'],
+                    processTree(input) {
+                        return fastbootTransform(input);
+                    }
+                }
+            }
+        }
+    },
+
+    included(app, parentAddon) {
+        this._super.included.apply(this, arguments);
+    }
+
+};
